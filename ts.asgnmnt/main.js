bba9c87b91999fcf12a3bff69083ca00f9c28d47
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2024-02-20 02:18:08 +0500

    update file

---
+++ ts.asgnmnt/main.js
@@ -0,0 +1,145 @@
+//1. //Qno1
+// installing node, typescript and vs code
+// Ans Done
+
+// 2.//Qno2
+//   let a = "SUBHAN";
+//   document.write(`Hello${a}, would you like to learn some python today?`);
+
+// 3.//Qno3
+// let personName = "Subhan";
+//   console.log("LowerCase:", personName.toLowerCase());
+//   console.log("UowerCase:", personName.toUpperCase());
+//   console.log("titleCase:", personName.replace(/\b\w/g, (c) => c.toUpperCase()));
+
+// 4. //Qno4
+//   let qoute =  "life is riding a bicycle to keep your balance yoiu must keep moving";
+//   let author = "ALbert Einstein";
+//   document.write(`"${author}" once said,"${qoute}"`);
+
+// 5. //Qno5;
+//   let qoute = "life is riding a bicycle to keep your balance you must keep moving";
+//   let famous_parson = "ALbert Einstein";
+//   let massage = `"${qoute}" - ${famous_parson}`;
+//   document.write(massage);
+
+// 6. //Qno7;
+//   let Addition = 5 + 3;
+//   document.write("Addition result:", Addition);
+
+//   let Subtraction = 10 - 2;
+//   document.write("Addition result:", Subtraction);
+
+//   let Multiplication = 4 * 2;
+//   document.write("Addition result:", Multiplication);
+
+// 7. // Qno8
+ // document.write(5 + 3);
+      // document.write(__________________ <br>);
+      // document.write(5 + 3);
+      // document.write(__________________ <br>);
+      // document.write(5 + 3);
+      // document.write(__________________ <br>);
+      // document.write(5 + 3);
+      // document.write(__________________ <br>);
+
+// 8. //Qno9
+//   let favnumber = 8;
+//   document.write(` My Favorite Number Is ${favnumber}`);
+
+//  9. // Qno10
+// auther:[ Subhan];
+// date:[friday,febraury,16,2024];
+
+// 10. //Qno11
+//   let friends = ["Subhan", "Shahid", "Adnan", "Dilbar"];
+//   document.write(`we are brothers an other mothers ${friends}`);
+
+// 11. // Qno12
+// let friends = ["Subhan", "Shahid", "Dilbar", "Adnan \t\n"];
+// let message = "we are mates";
+// // document.write(`${friends}${message}`);
+
+// 12. // Qno21
+// let c: String = "Subhan";
+// document.write("c")
+
+// 13. // Qno23
+// True Statements:
+// let car = "toyota";
+// document.write(car == "toyota");
+// let bike = "honda";
+// document.write(bike == "honda");
+// let aeroplane = "PIA";
+// document.write(aeroplane == "PIA");
+// let laptop = "Dell";
+// document.write(laptop == "Dell");
+// let cycle = "sohrab";
+// document.write(cycle == "sohrab");
+
+
+// // False Statements:
+
+// let carr = "toyota";
+// document.write(car == "audi");
+// let bikee = "honda";
+// document.write(bike == "yahma");
+// let aeroplanee = "PIA";
+// document.write(aeroplane == "indian");
+// let laptopp = "Dell";
+// document.write(laptop == "hp");
+// let cyclee = "sohrab";
+// document.write(cycle == "peeco");
+
+//  14. // Qno25
+// let alien_color = "green";
+
+//       if (alien_color == "green") {
+//         document.write("You earn 5 Points");
+//       } else {
+//         document.write("Version is fail");
+//       }
+
+//  15. // Qno26
+// let alien_color = "green";
+
+//       if (alien_color == "green") {
+//         document.write("Player earn 5 Points for shooting the alien.");
+//       } else {
+//         document.write("player just earned 10 points.");
+//   }
+
+// 16. // Qno27
+// let alien_color = "yellow";
+
+//       if (alien_color == "green") {
+//         document.write("Player earn 5 ");
+//       }
+//       if (alien_color == "yellow") {
+//         document.write("player aern 10.");
+//       }
+//       if (alien_color == "red") {
+//         document.write("player aern 15.");
+//       }
+
+// 17. // Qno28
+// var age = prompt("enter your age");
+//       if (age < 2) {
+//         document.write("person is a baby");
+//       } else if (age >= 2 && age < 4) {
+//         document.write("person is a toddler");
+//       } else if (age >= 4 && age < 13) {
+//         document.write("person is kid");
+//       } else if (age >= 13 && age < 20) {
+//         document.write("person is a teenager");
+//       } else if (age >= 20 && age < 65) {
+//         document.write("person is a adult");
+//       } else if (age > 65) {
+//         document.write("person is a elder");
+//       }
+
+// 18.  // Qno39
+// function city_country(multan, lahore, karachi) {
+//         document.write("Pakitan " + multan);
+//       }
+//       city_country("multan");
